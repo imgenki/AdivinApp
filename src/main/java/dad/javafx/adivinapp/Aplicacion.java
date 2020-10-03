216bb551522fafe038692243db661eeb40ea4dbe
imgenki <s.rguez00@gmail.com>
2020-10-03 10:05:50 +0100

    Refinando el código

--- src/main/java/dad/javafx/adivinapp/Aplicacion.java
+++ src/main/java/dad/javafx/adivinapp/Aplicacion.java
@@ -17,30 +17,29 @@ public class Aplicacion extends Application {
 	private Button comprobarButton;
 	private TextField numeroText;
 	private Label instruccionesLabel;
-	private Label aciertoComplementario;
 	private Alert acierto;
 	private Alert fallo;
 	private Alert numeroNoValido;
-	int numAdivinar = (int) ((Math.random() * 100) + 1);
+	int numAdivinar = (int) ((Math.random() * 100) + 1); // Generador del número a Adivinar
 	int numIntentos = 0;
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-
+		// Campo de texto
 		numeroText = new TextField();
 		numeroText.setPrefColumnCount(5);
 		numeroText.setPromptText("Introduce un número del 1 al 100");
 		numeroText.setMaxWidth(150);
-
+		// Botón
 		comprobarButton = new Button();
 		comprobarButton.setText("Comprobar");
 		comprobarButton.setOnAction(e -> onComprobarButtonAction(e));
 		comprobarButton.setDefaultButton(true);
-
+		// Label
 		instruccionesLabel = new Label();
 		instruccionesLabel.setTranslateY(-80);
 		instruccionesLabel.setText("Introduce un número del 1 al 100");
-
+		// Caja
 		VBox root = new VBox();
 		root.setSpacing(5);
 		root.setAlignment(Pos.CENTER);
@@ -57,31 +56,18 @@ public class Aplicacion extends Application {
 	private void onComprobarButtonAction(ActionEvent e) {
 
 		String textIntroducido = numeroText.getText();
-		String mayorMenor = "";
-
 		if (validate(textIntroducido)) {
 			int numIntroducido = Integer.parseInt(numeroText.getText());
 			if (numIntroducido > 0 && numIntroducido <= 100) {
 				if (numAdivinar == numIntroducido) {
 					numIntentos++;
-					acierto = new Alert(AlertType.INFORMATION);
-					acierto.setTitle("AdivinApp");
-					acierto.setHeaderText("¡Has ganado!");
-					acierto.setContentText("Sólo has necesitado " + numIntentos + " intentos" + "\n\nVuelve a jugar y hazlo mejor");
-					acierto.showAndWait();
-				} else if (numAdivinar != numIntroducido) {
+					acierto(numIntentos);
+				} else {
 					numIntentos++;
 					if (numIntroducido < numAdivinar)
-						mayorMenor = "mayor";
+						fallo("mayor", numIntroducido);
 					else
-						mayorMenor = "menor";
-					
-					fallo = new Alert(AlertType.WARNING);
-					fallo.setTitle("AdivinApp");
-					fallo.setHeaderText("¡Has fallado!");
-					fallo.setContentText("El número a adivinar es " + mayorMenor + " que " + numIntroducido);
-
-					fallo.showAndWait();
+						fallo("menor", numIntroducido);
 				}
 			} else {
 				numeroNoValido();
@@ -91,14 +77,11 @@ public class Aplicacion extends Application {
 		}
 	}
 
+	// Validador
 	private boolean validate(String text) {
 		return text.matches("[0-9]*");
 	}
-
-	public static void main(String[] args) {
-		launch(args);
-	}
-
+	// Ventana de error
 	public void numeroNoValido() {
 		numeroNoValido = new Alert(AlertType.ERROR);
 		numeroNoValido.setTitle("AdivinApp");
@@ -106,6 +89,31 @@ public class Aplicacion extends Application {
 		numeroNoValido.setContentText("El número introducido no es válido");
 
 		numeroNoValido.showAndWait();
+	}
+	
+	// Ventana en caso de acierto
+	public void acierto(int numIntentos) {
+		acierto = new Alert(AlertType.INFORMATION);
+		acierto.setTitle("AdivinApp");
+		acierto.setHeaderText("¡Has ganado!");
+		acierto.setContentText("Sólo has necesitado " + numIntentos + " intentos" + "\n\nVuelve a jugar y hazlo mejor");
+
+		acierto.showAndWait();
+	}
+	
+	// Ventana en caso de fallo
+	public void fallo(String mayorMenor, int numIntroducido) {
+		fallo = new Alert(AlertType.WARNING);
+		fallo.setTitle("AdivinApp");
+		fallo.setHeaderText("¡Has fallado!");
+		fallo.setContentText("El número a adivinar es " + mayorMenor + " que " + numIntroducido);
+
+		fallo.showAndWait();
+
+	}
+
+	public static void main(String[] args) {
+		launch(args);
 	}
 
 }
