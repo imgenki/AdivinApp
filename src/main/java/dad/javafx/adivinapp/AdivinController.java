6ffa61034db8a257f9d98caacd3ff1c2013a1bfd
imgenki <s.rguez00@gmail.com>
2020-12-08 15:26:44 +0000

    Update AdivinController.java

--- src/main/java/dad/javafx/adivinapp/AdivinController.java
+++ src/main/java/dad/javafx/adivinapp/AdivinController.java
@@ -1,4 +1,5 @@
 package dad.javafx.adivinapp;
+
 import javafx.event.ActionEvent;
 import javafx.util.converter.NumberStringConverter;
 
@@ -9,6 +10,7 @@ public class AdivinController {
 	public AdivinController() {
 		view.getComprobarButton().setOnAction(e -> onComprobarButtonAction(e));
 	}
+
 	public AdivinView getView() {
 		return view;
 	}
@@ -18,29 +20,30 @@ public class AdivinController {
 	}
 
 	private void onComprobarButtonAction(ActionEvent e) {
-	model.setNumIntroducido(Integer.parseInt(view.getNumeroText().textProperty().get()));
-	
-	String textIntroducido = view.getNumeroText().getText();
-	if (validate(textIntroducido)) {
-		int numIntroducido = Integer.parseInt(view.getNumeroText().getText());
-		if (numIntroducido > 0 && numIntroducido <= 100) {
-			if (model.getNumAdivinar() == model.getNumIntroducido()) {
-				model.numIntentosProperty().add(1);
-				view.acierto(model.getNumIntentos());
+		model.setNumIntroducido(Integer.parseInt(view.getNumeroText().textProperty().get()));
+
+		String textIntroducido = view.getNumeroText().getText();
+		if (validate(textIntroducido)) {
+			int numIntroducido = Integer.parseInt(view.getNumeroText().getText());
+			if (numIntroducido > 0 && numIntroducido <= 100) {
+				if (model.getNumAdivinar() == model.getNumIntroducido()) {
+					model.numIntentosProperty().add(1);
+					view.acierto(model.getNumIntentos());
+				} else {
+					model.numIntentosProperty().add(1);
+					if (model.getNumIntroducido() < model.getNumAdivinar())
+						view.fallo("mayor", model.getNumIntroducido());
+					else
+						view.fallo("menor", model.getNumIntroducido());
+				}
 			} else {
-				model.numIntentosProperty().add(1);
-				if (model.getNumIntroducido() < model.getNumAdivinar())
-					view.fallo("mayor", model.getNumIntroducido());
-				else
-					view.fallo("menor", model.getNumIntroducido());
+				view.numeroNoValido();
 			}
 		} else {
 			view.numeroNoValido();
 		}
-	} else {
-		view.numeroNoValido();
 	}
-}
+
 	private boolean validate(String text) {
 		return text.matches("[0-9]*");
 	}
